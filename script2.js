5a88855204c64b1a4069eac94bd3eead2b32c785
Harkirpa <kirpa704@gmail.com>
2023-06-02 21:09:58 +0530

    changes

---
+++ script2.js
@@ -0,0 +1,32 @@
+//guess
+let inputvalue=document.querySelector('input');
+let bodies=document.getElementsByName('body');
+let output=document.getElementsByClassName('message');
+let marks=document.getElementsByClassName('score');
+let guess=document.getElementsByClassName('number');
+let higher=document.getElementsByClassName('highscore');
+let randomNum=parseInt(Math.random() * 100+ 1);
+console.log(randomNum);
+
+let counted= 100;
+function maincheck(){
+    console.log(inputvalue.value);
+    if(randomNum>inputvalue.value){
+        output[0].innerHTML=("Your guess is Low");
+        counted--;
+        marks[0].innerHTML=(counted);
+    }
+    else if(randomNum<inputvalue.value)
+    {
+        output[0].innerHTML=("Your guess is high");
+        counted--;
+        marks[0].innerHTML=(counted);
+    }
+    else{
+        output[0].innerHTML=("😀😀Hurray you won😀😀");
+        guess[0].innerHTML=inputvalue.value;
+        bodies.style.color="green";
+        higher[0].innerHTML=(counted);
+        marks[0].innerHTML=(counted);
+    }
+}
